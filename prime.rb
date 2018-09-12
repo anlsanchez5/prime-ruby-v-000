bef2817d62bc16dde05da925ac1e6628ca31c8bf
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-12 22:32:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(num)
     if num < 3
       return false
     elsif
-      range = (2..num-1).to_a
+      range = (2..num - 1).to_a
       range.any? {|x|  num % x == 0}
       return false
     else
