f8db59d27819246d77a913f9eb2444e6cf842118
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-09-12 22:04:31 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(num)
   for i in 2..num - 1
-    if num < 0
-      prime_flag = false
-    elsif num % i == 0 
+    if num < 0 || num % i == 0 
        prime_flag = false
     else
       prime_flag = true
